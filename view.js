7405616ba06ed66a67b505630e47ac36bc2a60de
Modyfyer <Modyfyer@users.noreply.github.com>
2019-03-12 12:10:11 -0700

    Add files via upload

---
+++ view.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    getAttribs();
+    getFields();
+    function getFields(){
+        
+    }
+    
+    function getAttribs(div){
+        var attribs = {};
+        attribs.$div = div;
+        return attribs;
+    }
+});
